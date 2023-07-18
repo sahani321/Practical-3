6c04e915f74a509d058b4188cb414a0c66b5c06f
sahani321 <43401244+sahani321@users.noreply.github.com>
2023-07-18 18:26:50 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,33 @@
+package com.Anudip.Thursday_Lap_Practical;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		
+//		WAP to accept a no From the user and display the multiplication table
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number");
+		 num = sc.nextInt();
+		
+		for(int i = 1; i<=10; i++) {
+			
+			System.out.println(num+" X "+i+ " = "+ num*i);
+			
+		}
+		
+	
+		
+		
+		
+		
+						
+		
+		
+		
+		
+		
+	}
+
+}
